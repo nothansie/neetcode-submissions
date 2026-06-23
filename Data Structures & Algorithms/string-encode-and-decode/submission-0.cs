d555268b206ff5b5db99b0a907b1c50ae69cd530
Hansie Scheepers <hansiedeveloper@gmail.com>
2026-06-23 19:15:53 +0200

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.cs
@@ -0,0 +1,27 @@
+public class Solution {
+
+    public string Encode(IList<string> strs) {
+        string encoded = "";
+        for(int i = 0; i < strs.Count; i++){
+            var currentStr = strs[i];
+            string indicator = currentStr.Length.ToString() + "#"; 
+            encoded = encoded + indicator + currentStr;
+        }
+        return encoded;
+    }
+
+    public List<string> Decode(string s) {
+        var result = new List<string>();
+        int slow = 0;
+        for(int i = 0; i < s.Length; i++){
+            if(s[i] == '#'){
+                var length = int.Parse(s[slow..i]);
+                var str = s[(i+1)..(i+1+length)];
+                result.Add(str);
+                slow = i+1+length;
+                i = slow;
+            }
+        }
+        return result;
+   }
+}
