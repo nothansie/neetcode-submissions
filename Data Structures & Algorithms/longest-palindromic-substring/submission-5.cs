51ed02dad39946997e2dcf4e44b58f71de05b023
Hansie Scheepers <hansiedeveloper@gmail.com>
2026-07-03 20:53:26 +0200

    Add: longest-palindromic-substring - submission-5

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-5.cs
@@ -0,0 +1,45 @@
+public class Solution {
+    public string LongestPalindrome(string s) {
+        if(s.Length == 1){
+            return s;
+        }
+        string longestPalindrome = "";
+        for(int i = 0; i < s.Length; i++){
+            int left = i;
+            int right = i;
+            while(left != 0 || right != s.Length - 1){
+                if(left > 0){
+                    left--;
+                }
+                var isAttemptOnePalindrome = false;
+                isAttemptOnePalindrome = isPalindrome(s[left..(right+1)]);
+                if(right < s.Length - 1){
+                    right++;
+                }
+                var isAttemptTwoPalindrome = isPalindrome(s[left..(right+1)]);
+                if(!isAttemptOnePalindrome && !isAttemptTwoPalindrome){
+                    break;
+                }
+            }
+        }
+        return longestPalindrome;
+
+        bool isPalindrome(string checkString){
+            bool isPalindrome = true;
+            int left = 0;
+            int right = checkString.Length - 1;
+            while(left < right){
+                if(checkString[left] != checkString[right]){
+                    isPalindrome = false;
+                    break;
+                }
+                left++;
+                right--;
+            }
+            if(isPalindrome && checkString.Length > longestPalindrome.Length){
+                longestPalindrome = checkString; 
+            }
+            return isPalindrome;
+        }
+    }
+}
